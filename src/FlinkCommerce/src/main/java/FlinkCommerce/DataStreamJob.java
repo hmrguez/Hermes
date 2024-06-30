2ba2613baf89cccaf399370f1804f61a8dc6577c
hmrguez <zealot.algo@gmail.com>
2024-06-30 14:06:51 -0400

    Now running flink locally and it works

--- src/FlinkCommerce/src/main/java/FlinkCommerce/DataStreamJob.java
+++ src/FlinkCommerce/src/main/java/FlinkCommerce/DataStreamJob.java
@@ -21,9 +21,11 @@ package FlinkCommerce;
 import Deserializer.JSONValueDeserializationSchema;
 import Dto.Transaction;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 
@@ -46,23 +48,19 @@ public class DataStreamJob {
         // to building Flink applications.
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        String topic = "temp";
+        String topic = "financial_transactions";
 
         KafkaSource<Transaction> source = KafkaSource.<Transaction>builder()
-                .setBootstrapServers("kafka:29092")
+                .setBootstrapServers("localhost:9092")
                 .setTopics(topic)
                 .setGroupId("flink-group")
                 .setStartingOffsets(OffsetsInitializer.earliest())
                 .setValueOnlyDeserializer(new JSONValueDeserializationSchema())
                 .build();
 
-        DataStream<Transaction> transactionStream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source")
-                .setParallelism(1);
+        DataStreamSource<Transaction> stream = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
-        transactionStream.map(value -> {
-            System.out.println("Received message: " + value);
-            return value;
-        }).addSink(new PrintSinkFunction<>());
+        stream.print();
 
         // Execute program, beginning computation.
         env.execute("Flink Java API Skeleton");
